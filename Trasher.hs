dc942c9d60a6596bf5aaa711e03079ff536c0479
Joel Lehtonen <joel.lehtonen@iki.fi>
2022-03-17 16:37:42 +0200

    Add withTrasher function which does cleanup at the end

--- Trasher.hs
+++ Trasher.hs
@@ -2,14 +2,14 @@ module Trasher where
 
 import Data.ByteString (ByteString)
 import System.Posix.Files.ByteString (removeLink)
-import Control.Concurrent (ThreadId, forkIO)
+import Control.Concurrent (ThreadId, forkIO, killThread)
 import Control.Concurrent.STM
 import Control.Monad (forever, guard)
 
 type Trash = ByteString -> STM ()
 
 -- |Create queue which just removes everything sent there 
-runTrasher :: IO (ThreadId, ByteString -> STM ())
+runTrasher :: IO (ThreadId, Trash)
 runTrasher = do
   q <- newTQueueIO
   tid <- forkIO $ forever $ do
@@ -18,4 +18,11 @@ runTrasher = do
       flushTQueue q
     mapM_ removeLink list
   pure (tid, writeTQueue q)
-  
+
+-- |Run trasher and kill thread when the action is finished
+withTrasher :: (Trash -> IO ()) -> IO ()
+withTrasher act = do
+  (tid, putter) <- runTrasher
+  act putter
+  killThread tid
+
