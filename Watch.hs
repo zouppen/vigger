c86307cfb6855ad26959da266ee0211e4fe05448
Joel Lehtonen <joel.lehtonen@iki.fi>
2022-03-21 21:23:31 +0200

    Expose only selected actions from Watch

--- Watch.hs
+++ Watch.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE RecordWildCards, OverloadedStrings #-}
-module Watch ( Watch(..)
+module Watch ( Watch(stopWatch, lastEnqueue)
              , startCapture
              , stopCapture
              , forkWatch
