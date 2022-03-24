415fbf3e4ba82fc17c37d810a277b091b6dd407c
Joel Lehtonen <joel.lehtonen@iki.fi>
2022-03-24 17:37:58 +0200

    First version which can actually grab videos via journal

--- src/JournalInterface.hs
+++ src/JournalInterface.hs
@@ -1,17 +1,23 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric, RecordWildCards #-}
 module JournalInterface (journalInterface) where
 
-import Data.Aeson
-import System.IO (Handle, hClose)
-import System.Process
-import qualified Data.ByteString as B
-import GHC.Generics
-import Data.Function (fix)
-import Control.Exception (bracket)
-import Data.Map.Strict (Map, (!?), keys, foldlWithKey)
+import Control.Concurrent.Async (forConcurrently)
+import Control.Concurrent.STM (atomically)
+import Control.Exception (try, throw, bracket)
 import Control.Monad (forever)
+import Data.Aeson
+import qualified Data.ByteString as B
+import Data.Function (fix)
+import Data.Map.Strict (Map, (!?), keys, foldlWithKey)
+import GHC.Generics
+import System.IO (Handle, hClose)
+import System.IO.Temp (emptySystemTempFile)
+import System.Process
 
+import Exceptions
+import Ffmpeg
 import Loader
+import Watch
 
 data Rpc = Rpc { method :: String
                , params :: Bool
@@ -21,7 +27,23 @@ instance FromJSON Rpc
 
 -- Handle incoming RPC messages from journalctl
 journalInterface :: String -> Map String TriggerOp -> IO ()
-journalInterface unit ops = bracket start stop loop
+journalInterface unit ops = bracket start stop $ \(_,h) -> forever $ do
+  Rpc{..} <- takeNextJsonLine h
+  let op = ops !? method
+  case (op, params) of
+    (Just TriggerOp{..}, True) -> do
+      motionStart
+      putStrLn $ "Motion started on " <> method
+    (Just TriggerOp{..}, False) -> do
+      -- Encode video and remove files afterwards
+      motion <- motionEnd
+      videos <- forConcurrently motion $ \Capture{..} -> do
+        outfile <- emptySystemTempFile "vigger.mp4"
+        composeVideo outfile captureFiles
+        atomically captureClean
+        pure outfile
+      putStrLn $ "Motion stopped on " <> method <> ". Got videos: " <> show videos
+    _ -> putStrLn $ "Ignored method " <> method
   where
     start = runJournal False unit
     stop (procH, h) = do
@@ -29,14 +51,14 @@ journalInterface unit ops = bracket start stop loop
       terminateProcess procH
       waitForProcess procH
       pure ()
-    loop (_, h) = forever $ do
-      x <- takeNextJsonLine h
-      print (x :: Rpc)
-
+      
 -- |Gets lines until a valid JSON element is found, ignoring other
--- input. In case of EOF we get IOException.
+-- input. In case of EOF ViggerStop is thrown.
 takeNextJsonLine :: FromJSON a => Handle -> IO a
-takeNextJsonLine h = fix $ \loop -> B.hGetLine h >>= maybe loop pure . decodeStrict   
+takeNextJsonLine h = fix $ \loop -> get >>= maybe loop pure . decodeStrict   
+  where get = do
+          line <- try $ B.hGetLine h
+          either (ioeConst $ throw ViggerStop) pure line
 
 -- |Run journalctl with given unit.
 runJournal :: Bool -> String -> IO (ProcessHandle, Handle)
