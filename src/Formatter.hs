0ca45a841cbbe48cf39c4b6a06bb0eef33bb05b6
Joel Lehtonen <joel.lehtonen@iki.fi>
2022-03-28 00:47:25 +0300

    Introduce Formatter which can gap fill templates

--- src/Formatter.hs
+++ src/Formatter.hs
@@ -1,45 +1,73 @@
 {-# LANGUAGE OverloadedStrings #-}
--- |Formatter with a template
-module Formatter where
+-- |Formatter with a template.
+module Formatter ( substitute
+                 , toSubstituter
+                 , f0
+                 , f1
+                 ) where
 
-import Data.Text (Text, unpack)
+import Control.Applicative
+import Data.Attoparsec.Combinator (lookAhead)
+import Data.Attoparsec.Text
 import Data.Map.Strict (Map, (!?), fromList)
-import qualified Data.Map.Strict as M
-import Control.Monad.Except
-
-data Token = Plain Text      -- ^Plain text
-           | Var Text [Text] -- ^Substitute variable
-           deriving (Show)
+import Data.Text (Text, unpack)
+import qualified Data.Text as T
 
 newtype Substituter = Substituter (Text -> Maybe SubstAct)
 
-newtype SubstAct = SubstAct ([Text] -> SubstituteMonad Text)
+newtype SubstAct = SubstAct ([Text] -> Parser Text)
 
 data Substitution = Substitution Text SubstAct
 
-type SubstituteMonad = Either String
+-- |Try to substitute variables in a template. Returns Right with the
+-- text when substitution succeeds or Left with the position where the
+-- error occured.
+substitute :: Substituter -> Text -> Either Int Text
+substitute subst template = case feed (parse (everything subst) template) "" of
+  Done i r -> if i == mempty then Right r else failed i
+  Fail i _ _ -> failed i
+  where failed i = Left (T.length template - T.length i)
 
-substitute :: Substituter -> [Token] -> SubstituteMonad Text
-substitute s xs = mconcat <$> traverse (substituteOne s) xs
+-- |Parses and substitutes variables on the go.
+everything :: Substituter -> Parser Text
+everything subst = mconcat <$> p
+  where
+    p = many (plain <|> s) <* endOfInput
+    s = var >>= varSubst subst
 
-substituteOne :: Substituter -> Token -> SubstituteMonad Text
-substituteOne _ (Plain a) = pure a
-substituteOne (Substituter s) (Var name args) = case s name of
-  Nothing  -> throwError $ "Unknown variable: " <> unpack name
-  Just (SubstAct a) -> a args
+plain :: Parser Text
+plain = takeText1before "{{"
+
+var :: Parser [Text]
+var = "{{" *> takeText1before (eitherP sep "}}") `sepBy1` sep <* "}}"
+  where sep = char '|'
+
+-- |Substitutes a variable from given list of arguments.
+varSubst :: Substituter -> [Text] -> Parser Text
+varSubst _ [] = fail "Empty variable name"
+varSubst (Substituter s) (name:xs) = case s name of
+  Nothing             -> fail $ "Unknown variable: " <> unpack name
+  Just (SubstAct act) -> act xs
+
+-- |This takes one or many chars as Text until it matches the given
+-- "end" parser. Doesn't consume "end".
+takeText1before :: Parser a -> Parser Text
+takeText1before end = takeMany1text anyCharBefore
+  where takeMany1text = fmap fst . match . skipMany1
+        anyCharBefore = eitherP (lookAhead end) anyChar >>= either (const empty) pure
 
 toSubstituter :: [Substitution] -> Substituter
 toSubstituter handlers = Substituter (m !?)
   where m = fromList $ map (\(Substitution k v) -> (k, v)) handlers
 
+-- |Argumentless replacement
 f0 :: Text -> Text -> Substitution
 f0 key value = Substitution key (SubstAct f)
-  where f :: [Text] -> SubstituteMonad Text
-        f [] = pure value
-        f _  = throwError $ "Too many arguments to " <> unpack key
+  where f [] = pure value
+        f _  = fail $ "No arguments were expected to " <> unpack key
 
-f1 :: Text -> (Text -> SubstituteMonad Text) -> Substitution
+-- |Replacement with one argument
+f1 :: Text -> (Text -> Parser Text) -> Substitution
 f1 key func = Substitution key (SubstAct f)
-  where f :: [Text] -> SubstituteMonad Text
-        f [arg] = func arg
-        f _  = throwError $ "Invalid number of arguments in " <> unpack key
+  where f [arg] = func arg
+        f _     = fail $ "Expecting exactly one argument to " <> unpack key
